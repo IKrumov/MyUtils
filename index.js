e7092a1304a6f141df2f936728c4ecf8f18a7482
Ivan Krumov <ikrumov@richsourcedev.com>
2018-12-01 07:27:57 +0200

    Fitness

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
@@ -8,6 +7,19 @@ const app = express()
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const datasourcePath = __dirname + '/app/data/location_history.json';
+const fitnessDatasourcePath = __dirname + '/app/data/fitness.json';
+const foodDatasourcePath = __dirname + '/app/data/food.json';
+
+function prettyStringify(json) {
+	var result = JSON.stringify(json,function(k,v){
+		if (k === 'data' ) return v;
+		if (k === 'locations' ) return v;
+		if(v instanceof Array) return JSON.stringify(v);
+		return v;
+	},2).replace(/\\"/g, '"').replace(/"\[/g, '[').replace(/]"/g, ']');
+	
+	return result;
+}
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
@@ -16,29 +28,16 @@ app.engine('.hbs', exphbs({
 }))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'app/views'))
-
-app.get('/', (request, response) => {
-  response.render('home')
-})
-
-app.get('/pomodoro', (request, response) => {
-  response.render('pomodoro')
-})
-
-app.get('/copytools', (request, response) => {
-  response.render('copytools')
-})
+app.get('/', (request, response) => { response.render('home') })
+app.get('/pomodoro', (request, response) => { response.render('pomodoro') })
+app.get('/fitness', (request, response) => { response.render('fitness') })
+app.get('/food', (request, response) => { response.render('food') })
 
 app.use("/app", express.static(__dirname + '/app'));
 app.use("/app/js", express.static(__dirname + '/app/js'));
 app.use("/app/data", express.static(__dirname + '/app/data'));
-
 app.use(favicon(path.join(__dirname, '/app/images', 'favicon.ico')));
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/poi', function (req, res) {
@@ -53,10 +52,11 @@ app.post('/poi', function (req, res) {
 		"link" : query.link
 	};
 	file.locations.push(newLoc);
-	fs.writeFile(datasourcePath, JSON.stringify(file), function (err) {
+	
+	fs.writeFile(datasourcePath, prettyStringify(file), function (err) {
 	  if (err) return console.log(err);
 	});
-	res.send(200, { id: id });
+	res.status(200).send({ id: id });
 })
 
 app.delete('/poi', function (req, res) {
@@ -70,9 +70,51 @@ app.delete('/poi', function (req, res) {
 			break;
 		}
 	}
-	fs.writeFile(datasourcePath, JSON.stringify(file), function (err) {
+	
+	fs.writeFile(datasourcePath, prettyStringify(file), function (err) {
 	  if (err) return console.log(err);
 	});
+	res.sendStatus(200);
+})
+
+app.post('/updateFoodItem', function (req, res) {
+	var query = req.body;
+	
+	var file = require(foodDatasourcePath);
+	
+	for(var i = 0; i < file.data.length; i ++)
+	{
+		if(file.data[i][0] == query[0])
+		{
+			file.data[i][3] = query[3];
+		}
+	}
+	
+	fs.writeFile(foodDatasourcePath, prettyStringify(file), function (err) {
+	  if (err) return console.log(err);
+	});
+
+	res.sendStatus(200);
+})
+
+app.post('/updateFitnessItem', function (req, res) {
+	var query = req.body;
+	
+	var file = require(fitnessDatasourcePath);
+	
+	for(var i = 0; i < file.data.length; i ++)
+	{
+		if(file.data[i][0] == query[0])
+		{
+			file.data[i][3] = query[3];
+			file.data[i][4] = query[4];
+		}
+	}
+	
+	fs.writeFile(fitnessDatasourcePath, prettyStringify(file), function (err) {
+	  if (err) return console.log(err);
+	});
+
 	res.sendStatus(200);
 })
 
